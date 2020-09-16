a6448f9c33215076809af743f237e250114598e8
alienmelon <nathalie.lawhead@gmail.com>
2020-09-16 13:46:18 -0700

    first commit...
    first commit

---
+++ source/EDIT_ME.js
@@ -0,0 +1,57 @@
+//the footer text, customize with your own footer message...
+var str_footer = "";
+var str_header = "welcome to the animated gif art dumb by alienmelon! visit <a href='http://tetrageddon.com/'>Tetrageddon</a>, <a href='http://unicornycopia.com/' target='_blank'>Unicornycopia</a>, and <a href='http://alienmelon.com/' target='_blank'>alienmelon.com</a> for more.";
+
+//first element of the array is the image filename. this is used for the large image and thumbnail.
+//second element is an optional image caption
+//set the caption to an empty string "" if you don't want one
+//
+//thumbnails and large images should be placed in the "images" folder.
+//thumbnails go into images>thumbnails and the large fullscreen images go into images>large
+//if you wish to change the path for images, then edit the path names in code.js (str_imagePath and str_imageThumbnail)
+
+var arr_images = [
+
+		["MeatDamage16.gif", "Test Caption"], 
+		["NUKE_LOOPED_ANIMATED.gif", "Test Caption"], 
+		["MeatDamage02.gif", "Test Caption"], 
+		["IMG_CHARACTER_RANDOMIZER.gif", "Test Caption"], 
+		["Explosion02_DONE.gif", "Test Caption"], 
+		["IMG_MM_ICON_DINOSAURS.gif", "Test Caption"], 
+		["SomeplaceElse.gif", "Test Caption"], 
+		["0_IMG_WELCOMETO.gif", "Test Caption"], 
+		["SEQ9.gif", "Test Caption"], 
+		["GJ_THUMB.gif", "Test Caption"], 
+		["BUN02_2.gif", "Test Caption"], 
+		["EnemyMeat_Shooter.gif", "Test Caption"], 
+		["WIN2.gif", "Test Caption"], 
+		["HYPNO.gif", "Test Caption"], 
+		["bluffs.gif", "Test Caption"], 
+		["runonce_30338.616518609226.png", ""],
+		["IMG_TITLE_FULL.gif", "Test Caption"], 
+		["Frogsplode01.gif", "Test Caption"], 
+		["Gob.gif", "Test Caption"], 
+		["ALL_MONKEY_PIECES_SML.gif", "Test Caption"], 
+		["IMG_MEATSLUG.gif", "Test Caption"], 
+		["BATMAN_COVER.png", ""],
+		["IMG_FOODS.gif", "Test Caption"], 
+		["GGoJ_ClarinetPoster.jpg", ""],
+		["Budycops.gif", "Test Caption"], 
+		["SEQ25_SHAKEBUTT.gif", "Test Caption"], 
+		["EnemyMeat_Idle.gif", "Test Caption"], 
+		["Monkey_Default_01.gif", "Test Caption"], 
+		["MeatDamage03.gif", "Test Caption"], 
+		["MeatDamage17.gif", "Test Caption"], 
+		["Spamlet.gif", "Test Caption"], 
+		["poster_meanwhile2.jpg", ""],
+		["Monkey_Malfunction_01.gif", "Test Caption"], 
+		["TITLE_ART.gif", "Test Caption"], 
+		["Meat_Big.gif", "Test Caption"], 
+		["FatLady.gif", "Test Caption"], 
+		["Background.gif", "Test Caption"], 
+		["Bunny_Attack_Animation.gif", "Test Caption"], 
+		["S2.gif", "Test Caption"], 
+		["GERBS.gif", "Test Caption"], 
+		["WALKING_FISH.gif", "Test Caption"]
+		
+]
---
+++ source/code.js
@@ -0,0 +1,124 @@
+//path to the images folder
+//if you do not want to use thumbnails use ths ame path for thumbnails as the large files
+var str_imagePath = "images/large/";
+var str_imageThumbnail = "images/thumbnails/";
+
+//fade in on load
+function imageLoadComplete(img){
+	img.style.visibility = "visible";
+	img.style.opacity = "1";
+	//div_footer.innerHTML = str_footer;
+}
+function imageLoadEffect(img, imgSrc){
+	//if loaded
+	if (img.complete) {
+		imageLoadComplete(img);
+	} else {
+		img.addEventListener('load', function(){
+		imageLoadComplete(img);
+	})
+	  img.addEventListener('error', function() {
+	      console.log("error loading image " + imgSrc);
+	  })
+	}
+}
+
+function openImage(imgInfo){
+		
+	var strImgPath = str_imagePath + imgInfo[0];
+	
+	//open the image, with next/prev arrows, clicking closes...
+	var _container = document.createElement("div");
+	_container.id = "_fullImage";
+	_container.className = "fullImage";
+	
+	document.body.appendChild(_container);
+	
+	//populate
+	_container.style.backgroundImage = "url(" + strImgPath +")";
+	
+	//caption
+	var _caption = document.createElement("div");
+	_caption.id = "_imageCaption";
+	_caption.className = "imageCaption";
+	_caption.innerHTML = imgInfo[1];
+	_container.appendChild(_caption);
+	
+	//close it (remove itself on click)...
+	_container.addEventListener('click', function(){
+		if(_container != null){
+			_container.parentNode.removeChild(_container);
+			//random background image
+			randBodyImage();
+		};
+	})
+	
+}
+
+function buildgrid(){
+		
+	for(let i = 0; i < arr_images.length; ++i){
+		//make grid
+		let imgInfo = arr_images[i]; //reference as "let" so it works in IE, passed to click event
+		var img = document.createElement("img");
+		var imgSrc = str_imageThumbnail + arr_images[i][0];
+		img.src = imgSrc;
+		//img.loading = "lazy";
+		img.className = 'images';
+		img.id = "_img_"+String(i);
+		//
+		//div_footer.innerHTML = "Loading: " + arr_images[i][0];
+		imageLoadEffect(img, imgSrc);
+		//
+		var span = document.createElement('span');
+		span.innerHTML = "<span class='imageDescription'>" + arr_images[i][1] + "</span>";
+		//
+		//the container for images and span
+		var _container = document.createElement("div");
+		_container.id = "_imageDiv_" + String(i);
+		_container.className = "image_thumb";
+		//add
+		_container.appendChild(img);
+		_container.appendChild(span);
+		div_parent.appendChild(_container);
+		//
+		//
+		_container.addEventListener('click', function(){
+			openImage(imgInfo);
+		})
+	}
+}
+
+//make a random image for the background
+function randBodyImage(){
+	//var strImg = str_imagePath + arr_images[Math.ceil(Math.random()*arr_images.length)-1][0];
+	//document.body.style.backgroundImage = "url('" + strImg + "')";
+}
+
+function init(){
+	
+	//build container...
+	div_parent = document.createElement("div");
+	div_parent.id = "container";
+	div_parent.className = "imageContainer";
+	document.getElementsByTagName('body')[0].appendChild(div_parent);
+	//build the footer (about text)
+	div_footer = document.createElement("div");
+	div_footer.id = "_footer";
+	div_footer.className = "footer";
+	div_footer.innerHTML = str_footer;
+	document.getElementsByTagName('body')[0].appendChild(div_footer);
+	//build the header (small tagline or image...)
+	div_header = document.createElement("div");
+	div_header.id = "_header";
+	div_header.className = "header";
+	div_header.innerHTML = str_header;
+	document.getElementsByTagName('body')[0].appendChild(div_header);
+	
+	//all the action happens here	
+	buildgrid();
+	
+	//random image for background...
+	randBodyImage();
+	
+}
